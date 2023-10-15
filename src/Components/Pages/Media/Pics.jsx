3398da1b18d6738aae1ba2ff563aa9f88a776a79
adawg2010 <jcebersole1@gmail.com>
2023-10-15 20:14:36 +0900

    1

--- src/Components/Pages/Media/Pics.jsx
+++ src/Components/Pages/Media/Pics.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Carousel from 'react-bootstrap/Carousel';
 function Pics() {
   return (
     <div>
-      
+      <Carousel>
+        <Carousel.Item>
+          <img src="manjifirstime.jpg" alt="manji" />
+        </Carousel.Item>
+      </Carousel>
     </div>
   )
 }
