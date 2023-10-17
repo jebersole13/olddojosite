a3e24f58f3ce608641bcb2f180b55d4a96df9838
adawg2010 <jcebersole1@gmail.com>
2023-10-17 20:01:44 +0900

    1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import {Link } from 'react-router-dom'
 
 
 function Navigation() {
@@ -21,7 +21,7 @@ function Navigation() {
           <Nav className="me-auto">
   
             <NavDropdown title="About" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/Ryukyu">Ryukyu Kempo</NavDropdown.Item>
+              <NavDropdown.Item href="/Ryukyu"> <Link>Ryukyu Kempo</Link> </NavDropdown.Item>
               <NavDropdown.Item href="/Kobudo">
            Kobudo
               </NavDropdown.Item>
