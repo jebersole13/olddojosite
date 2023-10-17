0a0de1d413513b468cbd830884d083a3d68226a7
adawg2010 <jcebersole1@gmail.com>
2023-10-17 19:50:54 +0900

    1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,8 +21,8 @@ function Navigation() {
           <Nav className="me-auto">
   
             <NavDropdown title="About" id="basic-nav-dropdown">
-              <NavDropdown.Item > <a href="./Ryukyu">Ryukyu Kempo</a> </NavDropdown.Item>
-              <NavDropdown.Item href="/Kobudo">
+              <NavDropdown.Item href="../Ryukyu">Ryukyu Kempo</NavDropdown.Item>
+              <NavDropdown.Item href="../Kobudo">
            Kobudo
               </NavDropdown.Item>
               <NavDropdown.Item href="./Bogu">Bogu Kumite</NavDropdown.Item>
