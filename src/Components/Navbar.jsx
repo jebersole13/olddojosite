e5915efd8392410508f337204bcdd3f4530be5c1
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-10-18 10:07:27 +0900

    1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
   return (
-    <Navbar className='$indigo text-white' >
+    <Navbar className='.bg-indigo text-white' >
       <Container >
         <Navbar.Brand href="/">     <img
               src="/ktklogo.png"
