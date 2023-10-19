70fa7eed99b2f2f03acdb83d9e34b6d1c790e53f
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-10-19 10:58:40 +0900

    1

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -12,30 +12,26 @@ function Contact() {
     className='Contact'
 
     >
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-      </Form.Text>
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-      </Form.Text>
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="name" placeholder="Full name" />
+        <Form.Text className="text-muted">
+         Last name first, first name last.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check if new" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
 </motion.div>
   )
 }
