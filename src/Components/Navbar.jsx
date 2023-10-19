e0d966a3cd7cf8c1e851f503a29801393a80bc3f
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-10-19 10:09:32 +0900

    1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from "react"
 import './Navbar.css'
 
 
 function Navigation() {
+  const [menuOpen, setMenuOpen]=useState(false);
   return (
     <nav>
+       <div className="menu" onClick={()=> {
+      setMenuOpen(!menuOpen)
+     }}>
+      <span></span>
+      <span></span>
+      <span></span>
+     </div>
+     <ul className={menuOpen ? "open" : ''}>
     <Navbar className='bg'>
       <Container >
         <Navbar.Brand href="/">     <img
@@ -20,7 +30,7 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-  
+  <li>
             <NavDropdown title="About" id="basic-nav-dropdown">
               <NavDropdown.Item href="/Ryukyu"> Ryukyu Kempo</NavDropdown.Item>
               <NavDropdown.Item href="/Kobudo">
@@ -30,19 +40,25 @@ function Navigation() {
               <NavDropdown.Item href="/Arnis">Arnis</NavDropdown.Item>
               <NavDropdown.Item href="/Instructor">Instructor</NavDropdown.Item>
             </NavDropdown>
+            </li>
+            <li>
             <Nav.Link href="Schedule">Schedule</Nav.Link>
+            </li>
+            <li>
             <NavDropdown title="Media" id="basic-nav-dropdown">
               <NavDropdown.Item href="/Video">Video</NavDropdown.Item>
               <NavDropdown.Item href="/Pics">
               Pictures
               </NavDropdown.Item>
               <NavDropdown.Item href="/Blog">Blog</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="Contact">Contact</Nav.Link>
+            </NavDropdown></li>
+            <li>
+            <Nav.Link href="Contact">Contact</Nav.Link></li>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </ul>
     </nav>
   );
 }
