d5c2ae5e7ec0bcdfe4be5abb9d6c1fad7394defc
adawg2010 <jcebersole1@gmail.com>
2023-10-22 19:26:34 +0900

    1

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,5 +1,3 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import React from 'react'
 import { motion } from "framer-motion"
 import './Contact.css'
@@ -12,28 +10,35 @@ function Contact() {
     className='Contact' >
       <h1>Contact us!</h1>
       <form name='contact' method='POST' data-netlify='true' onSubmit='submit'>
-      <Form className='contactForm' >
-        <input type="hidden" name='form-name' value='contact' />
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>First Name  </Form.Label>
-        <Form.Control type="name" placeholder="First name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Last Name </Form.Label>
-        <Form.Control type="name" placeholder="Last name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email </Form.Label>
-        <Form.Control type="email" placeholder="Email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicMessage">
-        <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder="Please write your message here."/>
-      </Form.Group>
-      <Button className='contactButton' variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+   
+        <div >
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-control" type="text" id="name" required />
+        </div>
+        <div >
+          <label className="form-label" htmlFor="phone">
+            Phone
+          </label>
+          <input className="form-control" type="phone" id="phone" required />
+        </div>
+        <div >
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input className="form-control" type="email" id="email" required />
+        </div>
+        <div >
+          <label className="form-label" htmlFor="message">
+            Message
+          </label>
+          <textarea className="form-control" id="message" required />
+        </div>
+        <button className="btn btn-danger" type="submit">
+        
+        </button>
+      
     </form>
 <br />
     <h6>We will get back to you within 48 hours of your message, if not sooner.</h6>
