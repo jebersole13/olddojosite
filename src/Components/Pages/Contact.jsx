44a7d4566a1c6792365155621a61c65733ac86fb
adawg2010 <jcebersole1@gmail.com>
2023-10-22 20:52:51 +0900

    1

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
     transition={{ duration: 0.5 }}
     className='Contact' >
       <h1>Contact us!</h1>
-      <form name='contact' method='POST' data-netlify='true' onSubmit='submit' 
+      <form name='contact' method='POST' data-netlify='true' onSubmit='submit'   data-netlify-honeypot="bot-field" 
       data-netlify-recaptcha="true" className='contactForm' action="/thank-you">
         <div >
         <input type="hidden" name="form-name" value="value_of_name_attribute_in_form_tag" />
