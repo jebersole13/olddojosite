f2858b8967a649c2ee7b4aae95fc8775ee8c4012
adawg2010 <jcebersole1@gmail.com>
2023-10-25 07:48:28 +0900

    1

--- src/Components/Pages/About/Arnis.jsx
+++ src/Components/Pages/About/Arnis.jsx
@@ -11,7 +11,7 @@ function Arnis() {
       <h1>Filipino Arnis</h1>
       <p>Arnis is a Filipnio Martial art focused on the use of the baton,sword,knife,and empty hand.
       Though not an Okinawan Martial Art, Arnis is a part of our curriculum. It has many compatiable and identical principals both with the Ryukyu Kempo and the Kobudo we practice.
-      Practicing Arnis can be taken as a separate martial art entirely, or as a compliment to Karate.</p>
+      Practicing Arnis can be taken as a separate martial art entirely, or as a complement to Karate.</p>
       <p>Grab a stick and give it a try.</p>
 
       <img src="arnisSticks.jpg" alt="kali" id='eskrima' />
