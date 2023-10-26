69605250aa523424ed2af93193454c6f6189bb4e
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-10-26 14:24:10 +0900

    be in black

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,23 +20,23 @@ function Navigation() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
   
-            <NavDropdown  title="About" id="basic-nav-dropdown"  >
-              <NavDropdown.Item className='text-primary' href="/Ryukyu"> Ryukyu Kempo</NavDropdown.Item>
-              <NavDropdown.Item className='text-primary' href="/Kobudo">
+            <NavDropdown  title="About" id="basic-nav-dropdown"   >
+              <NavDropdown.Item  href="/Ryukyu"> Ryukyu Kempo</NavDropdown.Item>
+              <NavDropdown.Item  href="/Kobudo">
            Kobudo
               </NavDropdown.Item>
-              <NavDropdown.Item className='text-primary' href="/Bogu">Bogu Kumite</NavDropdown.Item>
-              <NavDropdown.Item className='text-primary' href="/Arnis">Arnis</NavDropdown.Item>
-              <NavDropdown.Item className='text-primary' href="/Instructor">Instructor</NavDropdown.Item>
+              <NavDropdown.Item  href="/Bogu">Bogu Kumite</NavDropdown.Item>
+              <NavDropdown.Item  href="/Arnis">Arnis</NavDropdown.Item>
+              <NavDropdown.Item  href="/Instructor">Instructor</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link className='text-primary'  href="Schedule">Schedule</Nav.Link>
-            <NavDropdown className='text-primary' title="Media" id="basic-nav-dropdown" >
+            <Nav.Link   href="Schedule">Schedule</Nav.Link>
+            <NavDropdown  title="Media" id="basic-nav-dropdown" >
               <NavDropdown.Item href="/Video">Video</NavDropdown.Item>
               <NavDropdown.Item href="/Pics">
               Pictures
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link className='text-primary' href="Contact">Contact</Nav.Link>
+            <Nav.Link  href="Contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
