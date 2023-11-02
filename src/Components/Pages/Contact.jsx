424e641ff67734569ac889dd73ef20833ed40a9f
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-11-02 15:55:24 +0900

    fix?

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
     className='Contact' >
       <h1>Contact us!</h1>
       <form name='contact' method='POST' netlify onSubmit='submit'   
-      data-netlify-recaptcha="true" className='contactForm' action="https://kotekarate.com/Thanks">
+      data-netlify-recaptcha="true" className='contactForm'action='.' >
         <div >
         <input type="hidden" name="form-name" value="contact" />
           <label className="form-label" htmlFor="firstName">
