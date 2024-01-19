d4f20c52fbeb4e8026cba5a552b0d181fbbb4f01
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-01-19 15:46:43 +0900

    footer fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,11 +6,11 @@ function Footer() {
           <p class="m-0 text-center text-white">
             Copyright &copy; Kotekan Karate 2023-2024
          <br />
-      <a href="https://www.youtube.com/channel/UCTK-5tkQb7RztEofIadUvyw"><i class='bx bxl-youtube bx-border-circle  bx-md' id="socials" ></i></a>
+      <a href="https://www.youtube.com/channel/UCTK-5tkQb7RztEofIadUvyw"><i className='bx bxl-youtube bx-border-circle  bx-md' id="socials" ></i></a>
      
-      <a href="https://twitter.com/Kotekan23"><i class='bx bxl-twitter bx-border-circle  bx-md' id="socials" ></i></a>
+      <a href="https://twitter.com/Kotekan23"><i className='bx bxl-twitter bx-border-circle  bx-md' id="socials" ></i></a>
     
-      <a href="https://www.instagram.com/kotekankarate2523/"><i class='bx bxl-instagram bx-border-circle  bx-md' id="socials" ></i></a>
+      <a href="https://www.instagram.com/kotekankarate2523/"><i className='bx bxl-instagram bx-border-circle  bx-md' id="socials" ></i></a>
    
           </p>
                
