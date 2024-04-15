bc76d7a33ccbdc89ec6e6a5fe7797da11dd5150f
adawg2010 <jcebersole1@gmail.com>
2024-04-15 19:43:04 +0900

    video readd

--- src/Components/Pages/Media/Video.jsx
+++ src/Components/Pages/Media/Video.jsx
@@ -16,6 +16,10 @@ function Video() {
    * 
    * add
   */}
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/FEyNYgOiZko?si=u1gHRwQ2kiPQXjmP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/gQRIjui98Do?si=JXf6SX3jbTGSPHn6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/RNA_xS7zx5I?si=FYNaYC2VeFnl3ehT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/A5wCd-gSJFU?si=zAbbRTr8ceeyTsTf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
    
     </motion.div>
